2b6acbf6e0efadcb7e837f4cf7d49d6d04b73b9c
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-07-02 23:14:08 +0530

    svg added for copy button

--- docs/app.js
+++ docs/app.js
@@ -1,8 +1,29 @@
-const passwordLength = document.getElementById('length')
-const symbol = document.getElementById('symbol')
-const number = document.getElementById('number')
-const lowerCase = document.getElementById('lowercase')
-const upperCase = document.getElementById('uppercase')
-const passwordTxt = document.getElementById('password')
-function generate(){
+//DOM Elements
+const passwordLengthEl = document.getElementById('length')
+const symbolEl = document.getElementById('symbol')
+const numberEl = document.getElementById('number')
+const lowerCaseEl = document.getElementById('lowercase')
+const upperCaseEl = document.getElementById('uppercase')
+const passwordTxtEL = document.getElementById('password')
+const generateButton = document.getElementById('generatebutton')
+
+const randomFunc = {
+    lower: generateLowercase,
+    upper: generateUppercase,
+    number: generateNumber,
+    symbol: generateSymbol
+}
+//Generator functions
+function generateLowercase(){
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 97))
+}
+function generateUppercase(){
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 65))
+}
+function generateNumber(){
+    return String.fromCharCode(Math.floor(Math.random() * 10 + 48))
+}
+function generateSymbol(){
+    const symbols = '!@#$%^&*(){}[]=<>/,.'
+    return symbols[Math.floor(Math.random() * symbols.length)]
 }
