dbe10a71fbae3a907a91cfd9d8ac7ef58187e715
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-07-03 00:38:02 +0530

    comments added

--- docs/app.js
+++ docs/app.js
@@ -7,12 +7,15 @@ const upperCaseEl = document.getElementById('uppercase')
 const passwordTxtEL = document.getElementById('password')
 const generateButton = document.getElementById('generatebutton')
 
+//Objects of functions and his keys
 const randomFunc = {
     lower: generateLowercase,
     upper: generateUppercase,
     number: generateNumber,
     symbol: generateSymbol
 }
+
+//Copy to clipboard function
 document.getElementById('copy').addEventListener('click',function(){
     const textArea = document.createElement('textarea')
     const password = passwordTxtEL.textContent
@@ -25,9 +28,10 @@ document.getElementById('copy').addEventListener('click',function(){
     textArea.select()
     document.execCommand('copy')
     textArea.remove()
-    alert("Copied to Clipboard: ", password)
+    alert("Copied to Clipboard!")
 })
-//event listner to generate button
+
+//Adding Event listner to password generated button gives generated password
 generateButton.addEventListener('click', function () {
     //grabbing the password length value but is in string we need to convert in a number so i added a plus sign(+) after, you can multiply with one also 
     const length = +passwordLengthEl.value
@@ -39,17 +43,24 @@ generateButton.addEventListener('click', function () {
     //giving true or false and password length values to a function
     passwordTxtEL.textContent = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
 })
+
+
+//function for creating Password
 function generatePassword (lower, upper, number, symbol, length) {
     //empty string for password
     let generatedPassword = ''
+
     //adding all true or false values in typescount (true + true + true + false = 3)
     const typescount = lower + upper + number + symbol
+
     //creating a object for stroing true false values in a array and also filtering the array if any false values are in this
     const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item =>Object.values(item)[0])
+
     //if not boxes are checked means all are false that means types count are equals to 0
     if(typescount === 0){
         return ''//return empty string
     }
+
     //for loop for creating the password in a loop but here we are incrementing by typescount(checked boxes)
     for(let i = 0;i<length;i+=typescount){
         typesArr.forEach(type =>{
@@ -57,13 +68,19 @@ function generatePassword (lower, upper, number, symbol, length) {
             generatedPassword += randomFunc[funcName]()
         })
     }
+
+    //slicing the string by password lenght
     const finalPass =   generatedPassword.slice(0,length)
     return finalPass
 }
+
+
 //Generator functions
+
 //https://net-comber.com/charset.html
 function generateLowercase () {
-    return String.fromCharCode(Math.floor(Math.random() * 26 + 97))
+    //according to charset
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 97)) 
 }
 function generateUppercase () {
     return String.fromCharCode(Math.floor(Math.random() * 26 + 65))
