dfa861f527e5a5cde08c634c2ee776add8d3d997
Lakshay Kamat <lakshaykamat2048@gmail.com>
2022-07-02 18:43:02 +0530

    a little clone added

---
+++ docs/app.js
@@ -0,0 +1,8 @@
+const passwordLength = document.getElementById('length')
+const symbol = document.getElementById('symbol')
+const number = document.getElementById('number')
+const lowerCase = document.getElementById('lowercase')
+const upperCase = document.getElementById('uppercase')
+const passwordTxt = document.getElementById('password')
+function generate(){
+}
